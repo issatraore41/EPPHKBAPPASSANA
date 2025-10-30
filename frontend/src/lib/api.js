f662f5d8f497f52bf85be3e7db4886db95b4169d
issatraore41 <issa.traore41@gmail.com>
2025-10-30 18:11:05 +0000

    Change API_URL to use process.env for React

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,10 +1,8 @@
 // src/lib/api.js
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 export async function getData(endpoint) {
   const res = await fetch(`${API_URL}${endpoint}`);
-  if (!res.ok) {
-    throw new Error("Erreur API");
-  }
+  if (!res.ok) throw new Error("Erreur API");
   return res.json();
 }
